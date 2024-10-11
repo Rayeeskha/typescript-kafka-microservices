13411aaf3b2d818c1807147bcec381a9fe41758d
Rayees Khan <rayees.khan@aimbeyond.com>
2024-10-12 01:17:33 +0530

    catalog route test cases

--- catalog_service/src/services/catalog.service.ts
+++ catalog_service/src/services/catalog.service.ts
@@ -18,6 +18,9 @@ export class CatalogService{
 
     async updateProduct(input:any){
         const data = await this._repository.update(input);
+        if(!data.id){
+            throw new Error("unable to update product !");
+        }
         return data;
     }
 
